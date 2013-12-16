df459ac40ace717ab2614bae5d1201d186b1f188
Ryan Lane <ryan@ebookglue.com>
2013-12-16 21:17:11 +0000

    Refactored postgres connect url to use environment variable

--- deu.py
+++ deu.py
@@ -1,14 +1,16 @@
 import sqlalchemy as sa
+import sys, os
 
-engine = sa.create_engine("postgresql://localhost/postgres")
+base_postgres_url = os.environ.get('BASE_PG_URL')
+engine = sa.create_engine("%s/postgres" % base_postgres_url, echo=True)
 conn = engine.connect()
 conn.execute("commit")
 
-db_name = 'deuces2'
+db_name = sys.argv[1]
 
 conn.execute("create database %s;" % db_name)
 result = conn.execute("select oid from pg_database where datname='%s';" % db_name)
 oid = result.fetchone()[0]
 conn.close()
 
-import pdb; pdb.set_trace()
+print oid
